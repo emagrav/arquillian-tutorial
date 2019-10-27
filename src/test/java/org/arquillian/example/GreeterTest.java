7ab6b0cbf8fe739659c782f42882ac188f3f0a3b
grim <emagrav@gmail.com>
2019-10-27 12:43:39 +0100

    initial commit

---
+++ src/test/java/org/arquillian/example/GreeterTest.java
@@ -0,0 +1,14 @@
+package org.arquillian.example;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GreeterTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
