79c13cdf9247378b072fada828f1be0693b9bc0a
grim <emagrav@gmail.com>
2019-10-27 14:41:43 +0100

    version 1 with Weld ee embedded container

--- src/test/java/org/arquillian/example/GreeterTest.java
+++ src/test/java/org/arquillian/example/GreeterTest.java
@@ -2,13 +2,44 @@ package org.arquillian.example;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import javax.inject.Inject;
 
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
 public class GreeterTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	@Inject
+	Greeter greeter;
+    /*
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+            .addClass(Greeter.class)
+            .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+    */
+    @Deployment
+    public static JavaArchive createDeployment() {
+        JavaArchive jar = ShrinkWrap.create(JavaArchive.class)
+            //.addClass(Greeter.class)
+            .addClasses(Greeter.class, PhraseBuilder.class)
+            .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+        System.out.println(jar.toString(true));
+        return jar;
+    }
+
+    @Test
+    public void should_create_greeting() {
+        assertEquals("Hello, Earthling!",
+            greeter.createGreeting("Earthling"));
+        greeter.greet(System.out, "Earthling");
+    }
 
 }
